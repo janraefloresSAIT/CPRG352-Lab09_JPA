e33d0694928e4c5aa484f20ec5c91a57ef4d9d03
Flores <Flores@DESKTOP-RGPSDG2>
2022-07-12 16:31:51 -0600

    userDB.sql and javabean user / role created

---
+++ src/java/models/Role.java
@@ -0,0 +1,30 @@
+package models;
+
+public class Role {
+    private int rID;
+    private String rName;
+
+    public Role() {
+    }
+
+    public Role(int rID, String rName) {
+        this.rID = rID;
+        this.rName = rName;
+    }
+
+    public int getrID() {
+        return rID;
+    }
+
+    public void setrID(int rID) {
+        this.rID = rID;
+    }
+
+    public String getrName() {
+        return rName;
+    }
+
+    public void setrName(String rName) {
+        this.rName = rName;
+    }
+}
---
+++ src/java/models/User.java
@@ -0,0 +1,71 @@
+package models;
+
+public class User {
+    
+    private String email;
+    private boolean active;
+    private String fName;
+    private String lName;
+    private String password;
+    private int role;
+
+    public User() {
+    }
+    
+    public User(String email, boolean active, String fName, String lName, String password, int role) {
+        this.email = email;
+        this.active = active;
+        this.fName = fName;
+        this.lName = lName;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+}
