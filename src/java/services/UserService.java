13e7f6412087b429de633e32bd85ea10eafe1a1b
Flores <Flores@DESKTOP-RGPSDG2>
2022-08-11 15:32:03 -0600

    services package updated

--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -1,38 +1,50 @@
 package services;
 
+import dataaccess.RoleDB;
 import dataaccess.UserDB;
 import java.util.List;
+import models.Role;
 import models.User;
 
 public class UserService {
+
     public User get(String email) throws Exception {
         UserDB userDB = new UserDB();
         User user = userDB.get(email);
         return user;
     }
-    
+
     public List<User> getAll() throws Exception {
         UserDB userDB = new UserDB();
         List<User> users = userDB.getAll();
         return users;
     }
-    
-    public void insert(String email, boolean active, String fName, String lName, String password, int role) throws Exception {
-        User user = new User(email, active, fName, lName, password, role);
+
+    public void insert(String email, boolean active, String fName, String lName, String password, int roleID) throws Exception {
+        User user = new User(email, active, fName, lName, password);
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.getRole(email);
+        user.setRole(role);
         UserDB userDB = new UserDB();
         userDB.insert(user);
     }
-    
-    public void update(String email, boolean active, String fName, String lName, String password, int role) throws Exception {
-        User user = new User(email, active, fName, lName, password, role);
+
+    public void update(String email, boolean active, String fName, String lName, String password, int roleID) throws Exception {
         UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        user.setPassword(password);
+        user.setActive(active);
+        user.setFirstName(fName);
+        user.setLastName(lName);
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.getRole(email);
+        user.setRole(role);
         userDB.update(user);
     }
-    
+
     public void delete(String email) throws Exception {
-        User user = new User();
-        user.setEmail(email);
         UserDB userDB = new UserDB();
+        User user = userDB.get(email);
         userDB.delete(user);
     }
 }
